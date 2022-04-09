dbd4af70149c02fa0e05de0a27909da3eb5c9205
wqferr <wqferr@gmail.com>
2022-04-09 01:15:15 -0300

    More arg parsing

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,55 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(long, short, help = "Program executed once, before processing begins")]
-    start_program: String,
-
-    #[clap(long, short, help = "Program executed per line of input")]
-    line_program: String,
+    start_program: Option<String>,
 
     #[clap(long, short, help = "Program executed once, after processing ends")]
-    end_program: String,
+    end_program: Option<String>,
     
-    #[clap(long = "ifs", default_value = " ", help = "Input field separator")]
+    #[clap(long = "ifs", env = "IFS", default_value = " ", help = "Input field separator")]
     input_field_separator: String,
     
-    #[clap(long = "ofs", help = "Output field separator [default: same as --ifs]")]
+    #[clap(long = "ofs", env = "OFS", help = "Output field separator")]
     output_field_separator: Option<String>,
 
-    #[clap(long, short, help = "Whether to consider consecutive separator strings as empty fields")]
-    allow_empty_fields: bool
+    #[clap(long = "irs", env = "IRS", default_value = "\n", help = "Input record separator")]
+    input_record_separator: String,
+
+    #[clap(long = "ors", env = "ORS", help = "Output record separator")]
+    output_record_separator: Option<String>,
+
+    #[clap(long, short, help = "Whether to consider consecutive field separator strings as empty fields")]
+    allow_empty_fields: bool,
+
+    #[clap(
+        long,
+        short = 'A',
+        requires_all(&["allow-empty-fields"]),
+        help = "Whether to consider consecutive record separator strings as empty records"
+    )]
+    allow_empty_records: bool,
+
+    #[clap(
+        long,
+        short,
+        help = "Whether to skip the first record and interpret it as field names; TODO how can you reference these?"
+    )]
+    header: bool,
+
+    #[clap(help = "Program executed once per record")]
+    program: String,
+
+    #[clap(help = "File to be processed; omit to use standard input")]
+    input_file: Option<String>
 }
 
 fn main() {
     let mut args = Args::parse();
     if args.output_field_separator.is_none() {
         args.output_field_separator = Some(args.input_field_separator);
+    }
+
+    if args.output_record_separator.is_none() {
+        args.output_record_separator = Some(args.input_record_separator);
     }
 }
